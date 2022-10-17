0161a68c5085425ecd071dd847301768115c2af9
Husan2002 <karimovhusan531@gmail.com>
2022-10-17 11:44:39 +0600

    sdelali povtorenie v b103/day12_files.py/day14.pt/dobavlen esho pithon lile num_text.py v osnovnuyu deriktoriu

--- b103/day12_files.py
+++ b103/day12_files.py
@@ -2,6 +2,21 @@
 # print(f)
 # f.close()
 # 
+
+
+w = open('num_text.py', 'w', encoding='utf-8')
+w = w.write('print("hello world")')
+print(w)
+
+a = open('num_text.py', 'a', encoding='utf-8')
+a = a.write('\nprint("hi")')
+print(a)
+
+
+r = open('num_text.py', 'r', encoding='utf-8')
+r = r.read()
+print(r)
+
 # file = open('users.txt', 'a', encoding='utf-8')
 # file.write('hello aruuke\n')
 # 
@@ -10,48 +25,48 @@
 # print(ff)
 
 
-name_surname_thirdname = input('enter name,surname,patronymic please: ')
-birth_year = int(input('Enter birth year: '))
-location = input('enter name please: ')
-phone_number = int(input('enter phone_number of student: '))
+# name_surname_thirdname = input('enter name,surname,patronymic please: ')
+# birth_year = int(input('Enter birth year: '))
+# location = input('enter name please: ')
+# phone_number = int(input('enter phone_number of student: '))
 
-while 1:
-    print('1.add\n2.exit\n: ')
-    usl = int(input('FIO: '))
-    if usl == '1':
-        name = input('FIO: ').title()
-        if ' ' in name:
-            first_name = name.split()[0]
-            second_name = name.split()[1]
-            if len(first_name) > 2 and len(second_name) > 3:
-                try:
-                    phone = int(input('telefon: '))
-                    if len(str(phone)) > 9 or len(str(phone)) <= 12:
-                        try:
-                            birth_year = int(input('Enter birth year: '))
-                            if birth_year > 1930 and birth_year < 2023:
-                                location = input('enter location please: ')
-                                if len(location) > 3:
-                                    lst = [name, phone_number, birth_year, location]
-                                else:
-                                    print('enter correct location please..!')
-                                    continue
-                            else:
-                                print('WTF..? -_-')
-                                continue
-                        except ValueError:
-                            print('this value most be a number')
-                            continue
-                    else:
-                        print('enter all number')
-                        continue
-                except ValueError:
-                    print('this value most be number')
-                    continue
-                print(lst)
-                with open('users.txt', 'a', encoding='utf-8') as f:
-                    print(*lst, file = f)
-    elif usl == 2:
-        break
+# while 1:
+#     print('1.add\n2.exit\n: ')
+#     usl = int(input('FIO: '))
+#     if usl == '1':
+#         name = input('FIO: ').title()
+#         if ' ' in name:
+#             first_name = name.split()[0]
+#             second_name = name.split()[1]
+#             if len(first_name) > 2 and len(second_name) > 3:
+#                 try:
+#                     phone = int(input('telefon: '))
+#                     if len(str(phone)) > 9 or len(str(phone)) <= 12:
+#                         try:
+#                             birth_year = int(input('Enter birth year: '))
+#                             if birth_year > 1930 and birth_year < 2023:
+#                                 location = input('enter location please: ')
+#                                 if len(location) > 3:
+#                                     lst = [name, phone_number, birth_year, location]
+#                                 else:
+#                                     print('enter correct location please..!')
+#                                     continue
+#                             else:
+#                                 print('WTF..? -_-')
+#                                 continue
+#                         except ValueError:
+#                             print('this value most be a number')
+#                             continue
+#                     else:
+#                         print('enter all number')
+#                         continue
+#                 except ValueError:
+#                     print('this value most be number')
+#                     continue
+#                 print(lst)
+#                 with open('users.txt', 'a', encoding='utf-8') as f:
+#                     print(*lst, file = f)
+#     elif usl == 2:
+#         break
 
 
