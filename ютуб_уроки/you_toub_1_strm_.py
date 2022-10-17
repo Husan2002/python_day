7812b4808b234685083ed0f8fc1fe442b8a39a5c
Husan2002 <karimovhusan531@gmail.com>
2022-10-17 17:56:40 +0600

    added excasize in you_toube lessons/1

--- ютуб_уроки/you_toub_1_strm_.py
+++ ютуб_уроки/you_toub_1_strm_.py
@@ -24,11 +24,64 @@
 # else:
 #     print(f'{year} невесакосный год')
 
+"""как найти пириметр и площядь квадрата
+"""
+
+# def scuare(a):
+#     print(f'pirimetr kvadrata so storonoy {a} yavlaetsa {a * 4}')
+#     print(f'ploshad kvadrata yavlaetsa {a ** 2}')
+    
+
+# scuare(int(input('enter number: ')))
+
+"""среднее арефметическое число
+"""
+
+# summ = 0
+# lenn = 0
+# for i in range(1, 101):
+#     if i %2 == 0:
+#         summ += i
+#         lenn += 1
+
+# print(summ//lenn)
+
+"""как найти максимальный елимент без встроенных функции 'max and min'
+"""
+
+mac = 1
+for ma in range(1,101):
+    if ma > mac:
+        mac = ma
+print(mac)
+
+mic = 100
+for mi in range(1,101):
+    if mi < mic:
+        mic = mi
+print(mic)
 
 
-def scuare(a):
-    p = a * 4
-    s = a ** 2
-    return p,s
+# maxx = 1
 
-print(scuare(10))
+# for i in range(1, 101):
+#     if i % 3==0 and i % 2==0:
+#         if i > maxx:
+#             maxx = i
+    
+# print(maxx)
+
+
+
+"""найти наибольший общий делитель
+"""
+
+a, b = int(input('enter number ')),int(input('enter number '))
+
+while a != 0 and b != 0:
+    if a > b:
+        a %= b
+    else:
+        b %= a
+
+print(a + b)
