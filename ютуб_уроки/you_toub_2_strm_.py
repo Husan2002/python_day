ee5a6fccf2a54f42c7ed8556ca1277366591a4c7
Husan2002 <karimovhusan531@gmail.com>
2022-10-18 23:11:39 +0600

    add lesson from youtube2

--- ютуб_уроки/you_toub_2_strm_.py
+++ ютуб_уроки/you_toub_2_strm_.py
@@ -12,11 +12,42 @@
 #     print(lst)
 
 
-a = int(input('enter number: '))
-s = 0
+# a = int(input('enter number: '))
+# s = 0
 
-while a > 0:
-    s += a % 10
-    s = a // 10
+# while a > 0:
+#     s += a % 10
+#     s = a // 10
 
-print(s)
+# print(s)
+
+# sp = [7, 8, 4, 7, 10, 5, 10]
+# s1 = []
+# for x in sp:
+#     s1.append(x ** 2)
+
+# print(s1)
+
+# print([x ** 2 for x in sp])
+
+# s_input = input('enter word: ')
+# s = ''
+# for i in s_input:
+#     if i not in s and i != ' ':
+#         s += i
+
+# print(s)
+
+
+# pr = 'fdsagf gfdadf gtrhsf asdgth fergf gtyujtrg erghytujrefre'
+# print(len(pr.split()))
+
+
+# d_m_y = input('enter dd.mm.yyyy: ')
+# w_time = d_m_y.split('.')
+
+# day = w_time[0]
+# month = w_time[1]
+# year = w_time[2]
+
+# print(day,month,year)
