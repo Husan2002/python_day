7cb23966839e159930cfdbf9d35954ba3195f0e6
Husan2002 <karimovhusan531@gmail.com>
2022-10-18 17:07:17 +0600

    new function in git guide

--- b103/day14.py
+++ b103/day14.py
@@ -1,7 +1,24 @@
 # Github
-# git
+
+
+
+
 
 # git add .
+# git status
 # git commit -m'comment something'
+# git remote get-url origin
+# git push origin husan
+# git branch
+# git checkout -b husan
+# git gheckout husan
+# git branch
+# 
 # git push
+
+
+
+
+def get_hello_world() -> str:
+    return 'hello world'
 
